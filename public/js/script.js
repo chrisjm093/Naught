1c1e0a712b4be22f8272018325c8741e600198d7
Christopher Marti <chrisjm093@gmail.com>
2020-07-30 16:19:46 -0700

    Search funcitonality by date and place built, API GET request functioning for covidtracking.com by US and US state.

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,38 @@
+$(document).ready(()=>{
+ 
+})
+
+      $('#get-stats').click( ()=>{
+        let date = $('#select-date').val();
+        const state =$('#select-state').val();
+        
+        date = date.replace(/-/g, '');
+        var covidCall = "";
+        if(state === "us"){
+            covidCall = "us/" + date;
+        } else {
+            covidCall = "/states/" + state + "/" + date;
+        } 
+        covidApiCall(covidCall);
+
+        
+
+      })
 
 
-function covidApiCall() {
+function covidApiCall(covidCall) {
+    const queryURL = 'https://covidtracking.com/api/v1/' + covidCall + '.json';
 
-    //ajax call
-
-    GenerateStatsList(data)
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then( data =>{
+        console.log(data);
+    }).catch(err =>{
+        if( err ){
+            alert("As the pandemic is an ever evolving situation, Some dates or places may not work. Try searching a different place or date.")
+        }
+    })
 }
 
 function generateStatsList() {
@@ -24,4 +52,4 @@ function readArticleModal() {
 }
 
 
-module.exports = router;
+
